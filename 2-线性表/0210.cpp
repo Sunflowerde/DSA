6eb37c49e75fc0543e0e4a1fe668be127d85b603
Sunflower <ziwenxu@stu.pku.edu.cn>
2025-11-21 15:26:16 +0000

    Update

--- 2-线性表/0210.cpp
+++ 2-线性表/0210.cpp
@@ -16,33 +16,25 @@ int dfs(std::vector<std::string>& matrix, int row, int col, int rows, int cols) 
         if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
             int next_char_index = matrix[nr][nc] - 'A';
             if (!visited[next_char_index]) {
-                int next_step = dfs(matrix, nr, nc, rows, cols);
-                if (next_step > sub_step) {
-                    sub_step = next_step;
+                int steps = dfs(matrix, nr, nc, rows, cols);
+                if (steps > sub_step) {
+                    sub_step = steps;
                 }
             }
         }
     }
     visited[char_index] = false;
-    return 1 + sub_step; 
+    return 1 + sub_step;
 }
 
 int main() {
     int rows, cols;
-    int max_steps = 0;
     std::cin >> rows >> cols;
     std::vector<std::string> matrix(rows);
     for (int i = 0; i < rows; ++i) {
         std::cin >> matrix[i];
     }
-    for (int row = 0; row < rows; ++row) {
-        for (int col = 0; col < cols; ++col) {
-            int steps = dfs(matrix, row, col, rows, cols);
-            if (steps > max_steps) {
-                max_steps = steps;
-            }
-        }
-    }
+    int max_steps = dfs(matrix, 0, 0, rows, cols);
     std::cout << max_steps << std::endl;
     return 0;
 }
